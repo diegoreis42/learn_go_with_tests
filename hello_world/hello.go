d21b2b117657079205ddd277462d9183b2065c65
Diego Reis <reisdiego144@gmail.com>
2024-08-24 21:49:37 -0300

    TDD hello world

---
+++ hello_world/hello.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func Hello(name string) string {
+  if name == "" {
+    return "Hello, World"
+  }
+
+  return "Hello, " + name
+}
+
+func main() {
+  fmt.Println(Hello("Yawd"))
+}
+
