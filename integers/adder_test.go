c894bebcb63ad81e3d9cd5aa6a8954b2ce04e742
Diego Reis <reisdiego144@gmail.com>
2024-08-24 22:33:30 -0300

    learning basic arithmetic

---
+++ integers/adder_test.go
@@ -0,0 +1,25 @@
+package integers
+
+import "testing"
+
+func TestAdder(t *testing.T)  {
+  sum := Add(2, 2)
+  expected := 4
+
+  assertResult(t, sum, expected)
+}
+
+func TestSub(t *testing.T)  {
+  result := Sub(4, 2)
+  expected := 2
+
+  assertResult(t, result, expected)
+}
+
+func assertResult(t testing.TB, got, expected int)  {
+ t.Helper()
+
+  if got != expected {
+    t.Errorf("expected '%d' but got '%d'", expected, got)
+  } 
+}
